8d672bfa21ea35869cd93f27fab3e2bb26b6b94a
Antonio-Cristian-Nica-Dida <al438472@uji.es>
2025-07-08 16:58:01 +0200

    Updates

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { initAutoRetry } from './services/api'
-
-// Initialize auto-retry system for failed submissions
-initAutoRetry();
-
-createRoot(document.getElementById("root")!).render(<App />);
